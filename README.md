9ba8f7c3404ccca43fa7f74f14c842ed5ce87970
Maksim Rakitin <maxim.rakitin@gmail.com>
2017-10-06 12:11:02 -0400

    The code organized as a function. Photo updated. Description updated. Argparse to be added.

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # Passport photo preparation tool
 
-Requires a processed photo named `photo.jpg` of size 600x600 px.
+The script generates a vertical 6x4" image file with two photos in it from the provided file with tick guides to allow easier cutting of the printed photo
 
-### Useful links:
+Requires a processed photo of size 600x600 px.
+
+
+## Steps to create a passport photo from scratch:
+1. Take a picture of a person with a white background. Use flash and good lighting.
+2. Upload the picture to the photo cropping tool (see the link below), align and resize the photo as necessary.
+3. Crop and save the photo to your computer.
+4. Use image editing software (XnView, GIMP, Photoshop, etc.) to tune the levels of brightness and contrast to make the background white. Save the edited file.
+5. Run the script with the path of the saved image, the resulted photo for printing will be produced.
+6. Print it on your color printer or use CVS/Walmart/etc. printing service. Make sure the photo is printed without borders, or make sure the printout is scaled to produce two 2x2" passport-size photos.
+
+
+## Useful links:
 - https://travel.state.gov/content/passports/en/passports/photos.html: photo cropping tool to create 600x600 px input image.
 - https://www.xnview.com: correct levels, etc.
 - https://stackoverflow.com/a/10649311/4143531: paste image into another image.
-- https://stackoverflow.com/a/9204506/4143531: possible usage of scikit-image
+- https://stackoverflow.com/a/19303889/4143531: quality considerations.
+- https://stackoverflow.com/a/9204506/4143531: possible usage of scikit-image.
--- passport_photo.py
+++ passport_photo.py
@@ -5,53 +5,83 @@ Useful links:
 - https://travel.state.gov/content/passports/en/passports/photos.html: photo cropping tool to create 600x600 px input image.
 - https://www.xnview.com: correct levels, etc.
 - https://stackoverflow.com/a/10649311/4143531: paste image into another image.
-- https://stackoverflow.com/a/9204506/4143531: possible usage of scikit-image
+- https://stackoverflow.com/a/19303889/4143531: quality considerations.
+- https://stackoverflow.com/a/9204506/4143531: possible usage of scikit-image.
 """
+import os
 
 import numpy as np
 from PIL import Image
 
-data = np.ones((1800, 1200), dtype=np.uint32) * 255  # white canvas of vertical orientation 6x4"
-photo = Image.open("photo.jpg")
 
-photo_dim = photo.size  # (600, 600)
-required_dim = (600, 600)
-assert photo_dim == required_dim, 'Photo dimensions should be {}'.format(required_dim)
+def passport_photo(input_name='photo.jpg', output_name=None, background_color=255, canvas_size=(1800, 1200),
+                   start_left=300, start_top=200, tick_len=30, tick_color=0, offset=5,
+                   output_format='RGB', dpi=(300, 300), subsampling=0, quality=100):
+    """The function creates a vertical 6x4" image file with two photos in it from the provided file with tick guides
+    to allow easier cutting of the printed photo.
 
-start_left = 300
-start_top = 200
+    :param input_name: the name of the input image file (should be of size 600x600 px).
+    :param output_image: the name of output image file.
+    :param background_color: the code of the background color (0=black, 255=white).
+    :param canvas_size: the size of canvas (in pixels); (1800, 1200) - canvas of vertical orientation 6x4".
+    :param start_left: the horizontal position on the canvas to place the photo at (in pixels).
+    :param start_top: the vertical position on the canvas to place the photo at  (in pixels).
+    :param tick_len: the length of the ticks (in pixels).
+    :param tick_color: the color of the ticks (0=black, 255=white).
+    :param offset: the offset of the ticks from the corner of the inserted photo (in pixels).
+    :param output_format: the output format (RGB for JPEG).
+    :param dpi: the DPI of the resulted image.
+    :param subsampling: enable or disable subsampling (to change the quality of the resulted image).
+    :param quality: JPEG quality.
+    :return: the output name of the file.
+    """
 
-tick_len = 30
-tick_color = 0  # gray=200, black=0
+    if not os.path.isfile(input_name):
+        raise FileNotFoundError('The file "{}" cannot be found'.format(input_name))
 
-offset = 5
+    if not output_name:
+        name, ext = os.path.splitext(input_name)
+        output_name = '{}_6x4in{}'.format(name, ext)
 
-start_tops = [start_top, 2 * start_top + photo_dim[0]]
+    data = np.ones(canvas_size, dtype=np.uint32) * background_color
+    photo = Image.open(input_name)
 
-for start_top in start_tops:
-    data[start_top - tick_len - offset: start_top - offset,
-    start_left] = tick_color
-    data[start_top - tick_len - offset: start_top - offset,
-    start_left + photo_dim[1]] = tick_color
-    data[(start_top + photo_dim[0]) + offset: (start_top + photo_dim[0]) + offset + tick_len,
-    start_left] = tick_color
-    data[(start_top + photo_dim[0]) + offset: (start_top + photo_dim[0]) + offset + tick_len,
-    start_left + photo_dim[1]] = tick_color
+    photo_dim = photo.size
+    required_dim = (600, 600)
+    assert photo_dim == required_dim, 'Photo dimensions should be {}'.format(required_dim)
 
-    data[start_top,
-    start_left - tick_len - offset:start_left - offset] = tick_color
-    data[start_top + photo_dim[0],
-    start_left - tick_len - offset:start_left - offset] = tick_color
-    data[start_top,
-    (start_left + photo_dim[1] + offset): (start_left + photo_dim[1] + offset) + tick_len] = tick_color
-    data[start_top + photo_dim[0],
-    (start_left + photo_dim[1] + offset): (start_left + photo_dim[1] + offset) + tick_len] = tick_color
+    start_tops = [start_top, 2 * start_top + photo_dim[0]]  # to produce 2 vertically arranged photos
 
-# Get bits per point:
-# mode_to_bpp = {'1': 1, 'L': 8, 'P': 8, 'I;16': 16, 'RGB': 24, 'RGBA': 32, 'CMYK': 32, 'YCbCr': 24, 'I': 32, 'F': 32}
+    for start_top in start_tops:
+        # Vertical ticks:
+        data[start_top - tick_len - offset: start_top - offset,
+        start_left] = tick_color
+        data[start_top - tick_len - offset: start_top - offset,
+        start_left + photo_dim[1]] = tick_color
+        data[(start_top + photo_dim[0]) + offset: (start_top + photo_dim[0]) + offset + tick_len,
+        start_left] = tick_color
+        data[(start_top + photo_dim[0]) + offset: (start_top + photo_dim[0]) + offset + tick_len,
+        start_left + photo_dim[1]] = tick_color
 
-image = Image.fromarray(data)
-image = image.convert('RGB')
-for s in start_tops:
-    image.paste(photo, (start_left, s))
-image.save('result.jpg')
+        # Horizontal ticks:
+        data[start_top,
+        start_left - tick_len - offset:start_left - offset] = tick_color
+        data[start_top + photo_dim[0],
+        start_left - tick_len - offset:start_left - offset] = tick_color
+        data[start_top,
+        (start_left + photo_dim[1] + offset): (start_left + photo_dim[1] + offset) + tick_len] = tick_color
+        data[start_top + photo_dim[0],
+        (start_left + photo_dim[1] + offset): (start_left + photo_dim[1] + offset) + tick_len] = tick_color
+
+    image = Image.fromarray(data)
+    # image = image.convert(output_format)
+    image = image.convert(output_format)
+    for s in start_tops:
+        image.paste(photo, (start_left, s))
+    image.save(output_name, dpi=dpi, subsampling=subsampling, quality=quality)
+
+    return output_name
+
+
+if __name__ == '__main__':
+    passport_photo(input_name='photo.jpg')
